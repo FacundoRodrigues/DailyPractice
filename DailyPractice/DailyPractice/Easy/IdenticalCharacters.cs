fb5ebdc553cf755dde3a2b04a71bdfc633587b4c
Facundo Rodrigues <facuurodrigues@gmail.com>
2023-04-01 15:13:29 -0300

    01-04-23: HashSet excersive -> Identical Characters

---
+++ DailyPractice/DailyPractice/Easy/IdenticalCharacters.cs
@@ -0,0 +1,33 @@
+﻿namespace DailyPractice.Easy
+{
+    /*
+     * Check if a String Contains only Identical Characters
+     *
+     * Write a function that returns true if all characters in a string are identical and false otherwise.
+     *
+     *  Examples
+     *      isIdentical("aaaaaa") ➞ true
+     *      isIdentical("aabaaa") ➞ false
+     *      isIdentical("ccccca") ➞ false
+     *      isIdentical("kk") ➞ true
+     */
+
+    public static class IdenticalCharacters
+    {
+        //HasSet option
+        public static bool IsIdentical(string str)
+        {
+            HashSet<char> set = new HashSet<char>();
+
+            foreach (char c in str) set.Add(c);
+
+            return set.Count() == 1;
+        }
+
+        //Distinct option
+        public static bool IsIdenticalDistinctOption(string str)
+        {
+            return str.Distinct().Count() == 1;
+        }
+    }
+}
--- DailyPractice/DailyPractice/Program.cs
+++ DailyPractice/DailyPractice/Program.cs
@@ -1,7 +1,10 @@
-﻿using DailyPractice.Easy.HowManyVowels;
+﻿using DailyPractice.Easy;
+using DailyPractice.Easy.HowManyVowels;
 using DailyPractice.Easy.Recursividad;
 
-var factorial = ReturnTheFactorial.Factorial(5);
-var vowels = HowManyVowels.CountVowels("Celebration");
+//var factorial = ReturnTheFactorial.Factorial(5);
+//var vowels = HowManyVowels.CountVowels("Celebration");
+//var isIdentical = IdenticalCharacters.IsIdentical("kk");
+var isIdentical2 = IdenticalCharacters.IsIdenticalDistinctOption("aaaaaa");
 
-Console.WriteLine(vowels);
+Console.WriteLine(isIdentical2);
