28efe3c148d486a346c4bad1e6632ba9e35bbeee
Facundo Rodrigues <facuurodrigues@gmail.com>
2023-04-02 11:05:59 -0300

    02-04-23: Add exercise ShuffleName.cs

---
+++ src/BusinessLogic/Easy/ShuffleName.cs
@@ -0,0 +1,28 @@
+﻿namespace BusinessLogic.Easy
+{
+    public static class ShuffleName
+    {
+        /// <summary>
+        /// Create a function that accepts a string (of a person's first and last name) and returns a string with the first and last name swapped.
+        ///
+        /// Examples
+        ///     NameShuffle("Donald Trump") ➞ "Trump Donald"
+        ///     NameShuffle("Rosie O'Donnell") ➞ "O'Donnell Rosie"
+        ///     NameShuffle("Seymour Butts") ➞ "Butts Seymour"
+        ///
+        /// Notes
+        ///     There will be exactly one space between the first and last name.
+        ///     If you get stuck on a challenge, find help in the Resources tab.
+        ///     If you're really stuck, unlock solutions in the Solutions tab.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string NameShuffle(string str)
+        {
+            var stringArray = str.Split(' ');
+            Array.Reverse(stringArray, 0, 2);
+            var nameShuffled = string.Join(" ", stringArray);
+            return nameShuffled;
+        }
+    }
+}
