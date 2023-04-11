9c625c429b3b0f871a21971e1bbfb6124ff59519
Facundo Rodrigues <facundo.rodrigues@urbetrack.com>
2023-04-11 09:25:30 -0300

    11-04-23: Add exercise CheckIfTheSameCase.cs

---
+++ src/BusinessLogic/Easy/CheckIfTheSameCase.cs
@@ -0,0 +1,22 @@
+﻿namespace BusinessLogic.Easy
+{
+    public static class CheckIfTheSameCase
+    {
+        /// <summary>
+        /// Create a function that returns true if an input string contains only uppercase or only lowercase letters.
+        ///
+        /// Examples
+        ///     SameCase("hello") ➞ true
+        ///     SameCase("HELLO") ➞ true
+        ///     SameCase("Hello") ➞ false
+        ///     SameCase("ketcHUp") ➞ false
+        ///
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool SameCase(string str)
+        {
+            return str.All(x => char.IsLower(x)) || str.All(x => char.IsUpper(x));
+        }
+    }
+}
--- src/UI/Program.cs
+++ src/UI/Program.cs
@@ -6,6 +6,8 @@ using BusinessLogic.Easy.Recursividad;
 //var isIdentical = IdenticalCharacters.IsIdentical("kk");
 //var isIdentical2 = IdenticalCharacters.IsIdenticalDistinctOption("aaaaaa");
 //var monthName = MonthName.GetMonthName(13);
-var hacker = H4ck3rSp34k.HackerSpeakOptimized("javascript is cool");
+//var hacker = H4ck3rSp34k.HackerSpeakOptimized("javascript is cool");
 
-Console.WriteLine(hacker);
+var sameCase = CheckIfTheSameCase.SameCase("Hello");
+
+Console.WriteLine(sameCase);
