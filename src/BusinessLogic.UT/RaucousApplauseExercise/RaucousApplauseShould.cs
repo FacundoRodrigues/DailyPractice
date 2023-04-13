e41660a4e4c63461322eaf76b94d389e1ec94290
Facundo Rodrigues <facundo.rodrigues@urbetrack.com>
2023-04-13 10:21:18 -0300

    13-04-23: Add exercise CountClaps and Add unit test project

---
+++ src/BusinessLogic.UT/RaucousApplauseExercise/RaucousApplauseShould.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace BusinessLogic.UT.RaucousApplauseExercise
+{
+    public class RaucousApplauseShould
+    {
+        [Theory]
+        [InlineData("ClaClaClaClap!", 4)]
+        [InlineData("ClClClaClaClaClap!", 6)]
+        [InlineData("CCClaClClap!Clap!ClClClap!", 9)]
+        public void ShouldReturnExpectedClaps(string clapText, int expectedClaps)
+        {
+            var totalCountClaps = RaucousApplause.CountClaps(clapText);
+
+            Assert.Equal(totalCountClaps, expectedClaps);
+        }
+    }
+}
