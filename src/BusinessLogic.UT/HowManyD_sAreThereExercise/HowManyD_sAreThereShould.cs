9032cc14fb64ed0bbd0b35d4d3c2d12c03c00964
Facundo Rodrigues <facuurodrigues@gmail.com>
2023-04-15 15:53:11 -0300

    15-04-23: agrega HowManyD_sAreThere.cs and its test

---
+++ src/BusinessLogic.UT/HowManyD_sAreThereExercise/HowManyD_sAreThereShould.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLogic.Easy;
+using Xunit;
+
+namespace BusinessLogic.UT.HowManyD_sAreThereExercise
+{
+    public class HowManyD_sAreThereShould
+    {
+        [Theory]
+        [InlineData("The quick brown fox jumps over the lazy dog and the Dalmatian", 3)]
+        [InlineData("My friend Dylan got distracted in school.", 4)]
+        [InlineData("Debris was scattered all over the yard.", 3)]
+        [InlineData("The rodents hibernated in their den.", 3)]
+        public void ShouldReturnCountOfDs(string str, int expectedCount)
+        {
+            var resultCount = HowManyD_sAreThere.CountDs(str);
+
+            Assert.Equal(expectedCount, resultCount);
+        }
+    }
+}
---
+++ src/BusinessLogic/Easy/HowManyD_sAreThere.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BusinessLogic.Easy
+{
+    /// <summary>
+    /// Create a function that counts how many D's are in a sentence.
+    ///
+    /// Examples
+    ///     CountDs("The quick brown fox jumps over the lazy dog and the Dalmatian.") ➞ 3
+    ///     CountDs("My friend Dylan got distracted in school.") ➞ 4
+    ///     CountDs("Debris was scattered all over the yard.") ➞ 3
+    ///     CountDs("The rodents hibernated in their den.") ➞ 3
+    ///
+    /// Notes
+    ///     Your function must be case-insensitive.
+    ///     Remember to return the result.
+    /// </summary>
+    public static class HowManyD_sAreThere
+    {
+        public static string pattern = @"d";
+
+        //Seguir probando este Pattern para entender como scapea el and, dog y Dalmatian
+        public static string pattern2 = @"\b\w+d\b";
+
+        public static int CountDs(string str)
+        {
+            return Regex.Matches(str, pattern, RegexOptions.IgnoreCase).Count;
+        }
+    }
+}
