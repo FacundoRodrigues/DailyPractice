8e1b17a8599b86adab42bb57959e7caf09e62996
Facundo Rodrigues <facundo.rodrigues@urbetrack.com>
2023-04-22 12:36:18 -0300

    22-04-23: agrega EqualityCheck.cs and its test.

---
+++ src/BusinessLogic.UT/Medium/CheckEqualityExercise/CheckEqualityShould.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLogic.Medium;
+using Xunit;
+
+namespace BusinessLogic.UT.Medium.CheckEqualityExercise
+{
+    public class CheckEqualityShould
+    {
+        [Theory]
+        [InlineData(1, true, false)]
+        [InlineData(0, "0", false)]
+        [InlineData(1, 1, true)]
+        public void ShouldCompareTwoObjects(object firstObject, object secondObject, bool expectedResult)
+        {
+            var result = EqualityCheck.CheckEquality(firstObject, secondObject);
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
